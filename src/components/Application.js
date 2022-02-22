f11f9630fadb9be3fa13df148bef5066f8cc0656
NaderNasr <nadernasr7@gmail.com>
2022-02-22 15:39:25 -0500

    added daylist to render in app

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,26 @@
 import React from "react";
+import DayList from "./DayList";
 
 import "components/Application.scss";
 
 export default function Application(props) {
+  const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0
+    },
+  ];
   return (
     <main className="layout">
       <section className="sidebar">
@@ -12,7 +30,13 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            day={"Monday"}
+            setDay={day => console.log(day)}
+          />
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
@@ -21,7 +45,7 @@ export default function Application(props) {
 
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {/* Replace this with the schedule elements during the "The Scheduler" activity. */}
       </section>
     </main>
   );
