292b11d1beefcca438bae950c388ce3c36a68183
NaderNasr <nadernasr7@gmail.com>
2022-02-22 14:08:53 -0500

    added sentence formater function

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,12 +7,23 @@ export default function DayListItem({ name, spots, selected, setDay, full }) {
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected":selected,
     "day-list__item--full":full
-  })
+  });
+
+  const formatSpots = (spots) => {
+    if(spots === 1){
+      return `1 spot remaining`
+    }
+    if(spots === 0){
+      return 'no spots remaining '
+    } else {
+      return `${spots} spots remaining`
+    }
+  }
 
   return (
     <li className={dayClass} onClick={() => setDay(name)} selected={selected}>
       <h2 className="text--regular">{name}</h2>
-      <h3 className="text--light">{spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
   );
 }
