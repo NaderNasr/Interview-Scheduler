f82ab6d2ac78371bd2d933d2e996f467991de221
NaderNasr <nadernasr7@gmail.com>
2022-02-24 00:41:23 -0500

    create appointment view stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import Empty from './Empty';
+import Header from './Header';
+import Show from './Show';
 import './styles.scss';
 
-const Appointment = ({time}) => {
+const Appointment = ({ time, interview }) => {
 
-  const isTime = (time) => {
-    if (!time){
+  const isAppointmentAvailable = (time) => {
+    if (!time) {
       return 'No Appointment'
     } else {
-      return `Appointment at ${time}`
+      return `${time}`
     }
   }
-  
+
   return (
-    <article className="appointment">
-      {isTime(time)}
-    </article>
+    <>
+      <article className="appointment">
+      <Header time={isAppointmentAvailable(time)} />
+      {interview ? <Show student={interview.student} interviewer={interview.interviewer.name} /> : <Empty />}
+      </article>
+      
+    </>
   )
 }
 
