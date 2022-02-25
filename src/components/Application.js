f41f53e5af602ef05947f5ac0f87f389801dd6f4
NaderNasr <nadernasr7@gmail.com>
2022-02-24 22:51:11 -0500

    comment out unused code

--- src/components/Application.js
+++ src/components/Application.js
@@ -47,7 +47,7 @@ const appointments = [
 
 const Application = (props) => {
   const [dayName, setDayName] = useState([]);
-  const [interviewerName, setInterviewerName] = useState("");
+  // const [interviewerName, setInterviewerName] = useState("");
 
   useEffect(() => {
     axios.get(`/api/days`)
@@ -61,13 +61,13 @@ const Application = (props) => {
   }, [])
 
 
-  const interviewers = [
-    { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-    { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-    { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-    { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-    { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-  ];
+  // const interviewers = [
+  //   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  //   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  //   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  //   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  //   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+  // ];
 
   // const days = [
   //   {
