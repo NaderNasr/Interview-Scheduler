07d5a09bdaf3dfc9c442c3b7f77758a65a98ae14
NaderNasr <nadernasr7@gmail.com>
2022-02-26 15:48:30 -0500

    imported Form and implemented useVisualMode custom hook to Show and Form components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,9 +2,26 @@ import React from 'react';
 import Empty from './Empty';
 import Header from './Header';
 import Show from './Show';
+import Form from './Form';
+import useVisualMode from "hooks/useVisualMode";
+
 import './styles.scss';
 
-const Appointment = ({ time, interview }) => {
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
+
+
+const Appointment = ({ time, interview, interviewers }) => {
+
+  const {
+    mode,
+    transition,
+    back
+  } = useVisualMode(
+    interview ? SHOW : EMPTY
+  );
 
   const isAppointmentAvailable = (time) => {
     if (!time) {
@@ -17,10 +34,24 @@ const Appointment = ({ time, interview }) => {
   return (
     <>
       <article className="appointment">
-      <Header time={isAppointmentAvailable(time)} />
-      {interview ? <Show student={interview.student} interviewer={interview.interviewer.name} /> : <Empty />}
+        <Header time={isAppointmentAvailable(time)} />
+        {/* {interview ? <Show student={interview.student} interviewer={interview.interviewer.name} /> : <Empty />} */}
+        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+        {mode === SHOW && (
+          <Show
+            student={interview.student}
+            interviewer={interview.interviewer.name}
+          />
+        )}
+        {mode === CREATE && (
+        <Form
+          interviewerList={interviewers}
+          onCancel = {back}
+        />)}
+
+
       </article>
-      
+
     </>
   )
 }
