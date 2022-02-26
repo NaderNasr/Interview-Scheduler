dd55eaf74df6af30571fc8f837360de662602882
NaderNasr <nadernasr7@gmail.com>
2022-02-26 15:51:12 -0500

    custom hook for Appointments

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  //transition
+  function transition(current, replace = false) {
+    if (!replace) {
+      setMode(current)
+      history.push(current);
+    } 
+      setMode(current); 
+  }
+  function back() {
+    if (history.length > 1) {
+      const previous = [...history];
+      previous.pop();
+      setMode(previous[previous.length - 1]);
+      setHistory(previous);
+    }
+  }
+
+
+  return { mode, transition, back };
+}
+
