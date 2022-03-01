7705cacae1651fb75b60b24a11b88feb444b3988
NaderNasr <nadernasr7@gmail.com>
2022-02-28 21:42:31 -0500

    add edit feature for name and interviewer

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -2,10 +2,10 @@ import Button from 'components/Button';
 import InterviewerList from 'components/InterviewerList';
 import React, { useState } from 'react';
 
-const Form = ({ student, interviewerList, onCancel, onSave, bookInterview }) => {
+const Form = ({ interviewerList, onCancel, onSave, name, interviewerSelected }) => {
 
-  const [studentName, setStudentName] = useState(student || "");
-  const [interviewerID, setInterviewerID] = useState(interviewerList || null);
+  const [studentName, setStudentName] = useState(name || "");
+  const [interviewerID, setInterviewerID] = useState(interviewerSelected || null);
   const [noNameError, setNoNameError] = useState("");
   const [noInterviewerError, setNoInterviewerError] = useState("");
 
@@ -22,13 +22,11 @@ const Form = ({ student, interviewerList, onCancel, onSave, bookInterview }) => 
 
   const validate = () => {
     if (studentName === '') {
-      
       return setNoNameError("Student name cannot be blank");
-      
     }
     // const len = InterviewerList
-    console.log(InterviewerList.length)
-    console.log(interviewerID.length)
+    // console.log(InterviewerList.length)
+    // console.log(interviewerID.length)
 
     if (interviewerID.length) {
       return setNoInterviewerError("Please pick an interviewer");
@@ -38,17 +36,16 @@ const Form = ({ student, interviewerList, onCancel, onSave, bookInterview }) => 
     onSave(studentName, interviewerID);
   }
 
-
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
-        <section className="appointment__validation">{noInterviewerError}</section>
+          <section className="appointment__validation">{noInterviewerError}</section>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
-            placeholder="Enter Student Name"
+            placeholder={"Enter Student Name"}
             onChange={(event) => setStudentName(event.target.value)}
             value={studentName}
           />
@@ -65,7 +62,7 @@ const Form = ({ student, interviewerList, onCancel, onSave, bookInterview }) => 
           <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={validate}>Save</Button>
         </section>
-        
+
       </section>
     </main>
   )
