d593e8c296873b5e74e3eb21d62ac9a93ac56573
NaderNasr <nadernasr7@gmail.com>
2022-03-01 00:05:32 -0500

    moodified history when replace is false, fix the double click to go back

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,13 +3,15 @@ import { useState } from "react";
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
+
   //transition
-  function transition(current, replace = false) {
+  function transition(current, replace) {
     if (!replace) {
-      setMode(current)
-      history.push(current);
+      // setMode(current)
+      // history.push(current);
+      setHistory(prev => ([...prev, mode]));
     } 
-      setMode(current); 
+    setMode(current); 
   }
   function back() {
     if (history.length > 1) {
